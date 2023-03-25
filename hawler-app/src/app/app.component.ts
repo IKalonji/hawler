6880d9fa587f5917622b84ad4319d963bb82b69e
IKalonji <IKalonji@student.wethinkcode.co.za>
2023-03-26 00:38:23 +0200

    Based frontend created, function logic to be added.

--- hawler-app/src/app/app.component.ts
+++ hawler-app/src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'hawler-app';
+  navItems = [
+    {
+      label: 'hawler',
+    }
+  ]
+  chains: Chain[]= [
+    {
+      name: "Gnosis",
+      chainId: "0xsum",
+      contract: ""
+    },
+    {
+      name: "Scroll",
+      chainId: "0xsum",
+      contract: "",
+    },
+    {
+      name: "Polygon",
+      chainId: "0xsum",
+      contract: ""
+    },
+    {
+      name: "Mantle",
+      chainId: "0xsum",
+      contract: ""
+    }
+  ];
+  selectedChain: Chain = {name:"",chainId:"", contract: ""};
+  amount:Number = 0
+  password: String = "";
+
+  contract: String = "";
+  toAddress: String = "";
+  passwordWithdraw: String = "";
+  
+  loggedIn = false;
+
+  constructor(){
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  connectWallet(){}
+
+  sendfunds(){}
+
+  withdrawfunds(){}
+
+}
+
+interface Chain {
+  name: string,
+  chainId: string,
+  contract: string
 }
--- hawler-app/src/app/app.module.ts
+++ hawler-app/src/app/app.module.ts
@@ -3,12 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { MenubarModule } from 'primeng/menubar';
+import { DropdownModule } from 'primeng/dropdown';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { SplitterModule } from 'primeng/splitter';
+import { TabViewModule } from 'primeng/tabview';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+
+import {FormsModule} from '@angular/forms'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MenubarModule,
+    DropdownModule,
+    FormsModule,
+    ButtonModule,
+    CardModule,
+    SplitterModule,
+    TabViewModule,
+    InputNumberModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
