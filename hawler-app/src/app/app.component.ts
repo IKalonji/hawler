9fe9c9ca5393bced72964ed85f6ab6a241bf3e26
IKalonji <IKalonji@student.wethinkcode.co.za>
2023-03-26 16:33:41 +0200

    Update to frontend, added event to contracts.

--- hawler-app/src/app/app.component.ts
+++ hawler-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContractService } from './contract.service';
 
 @Component({
   selector: 'app-root',
@@ -16,36 +17,37 @@ export class AppComponent implements OnInit{
     {
       name: "Gnosis Chiado Testnet",
       chainId: "10200",
-      contract: ""
+      contract: "0xdd7a3Fb3dAaEf50e1F693A5c780c784De0eD7fE6"
     },
     {
       name: "Scroll Alpha Testnet",
       chainId: "534353",
-      contract: "",
+      contract: "0xaF52Fe8cb0745063Dfc8771929440e2F976a73C4",
     },
     {
       name: "zkEVM Testnet",
       chainId: "1442",
-      contract: ""
+      contract: "0x1D4d13B3cbc4C36506645cDF3b5c8826AB495969"
     },
     {
       name: "Mantle Testnet",
       chainId: "5001",
-      contract: ""
+      contract: "0x0FC4262580c16ef00689157E264500f9401fE684"
     }
   ];
   selectedChain: Chain = {name:"",chainId:"", contract: ""};
   amount:Number = 0
-  password: String = "";
+  password: string = "";
+  confirmPassword: string = "";
 
-  contract: String = "";
-  toAddress: String = "";
-  passwordWithdraw: String = "";
+  contract: string = "";
+  toAddress: string = "";
+  passwordWithdraw: string = "";
   
   loggedIn = false;
   wallet: string = "Connect Wallet";
 
-  constructor(){
+  constructor(private service:ContractService){
   }
 
   ngOnInit(): void {
@@ -66,21 +68,49 @@ export class AppComponent implements OnInit{
 
     this.wallet = accounts[0];  
 
-    const correctChain = window.ethereum.networkVersion == this.selectedChain.chainId
+    const correctChain = this.correctChain();
 
     if(!correctChain){
       console.log(window.ethereum.networkVersion);
       alert("Please switch to the correct chain in your Wallet! Should be: " + this.selectedChain.name + " with chainId "+this.selectedChain.chainId);
+      this.wallet = "Connect Wallet";
       return;
     }
-
     this.loggedIn = true;
-    
   }
 
-  sendfunds(){}
+  correctChain(){
+    return window.ethereum.networkVersion == this.selectedChain.chainId
+  }
 
-  withdrawfunds(){}
+  correctPasswordFormat(type:string){
+    if (type == 'send'){
+      return this.password == this.confirmPassword && this.password.length == 8;
+    }
+    if (type == 'withdraw'){
+      return this.passwordWithdraw.length == 8
+    }
+    return false;
+  }
+
+  sendfunds(){
+    if(this.amount > 0 && this.correctPasswordFormat('send') && this.correctChain()){
+      const _address = this.service.deposit(this.password, this.amount, this.selectedChain.contract);
+      alert("Transaction confirmed, withdrawal addres: " + _address + "Provide the address and password to the receiver.")
+      return;
+    }
+    alert("Input details are incorrect! Check and resubmit.")
+  }
+
+  withdrawfunds(){
+    if(this.contract != "" && this.passwordWithdraw.length == 8 && this.correctChain()){
+      let withdrawalAddr = this.toAddress != "" ? this.toAddress : this.wallet; 
+      const success = this.service.withdraw(this.password, withdrawalAddr, this.contract);
+      alert("Transaction confirmed with status: " + success);
+      return;
+    }
+    alert("Input details are incorrect! Check and resubmit.")
+  }
 
 }
 
--- hawler-contracts/Hawler.sol
+++ hawler-contracts/Hawler.sol
@@ -6,13 +6,14 @@ pragma solidity ^0.8.17;
 
 contract Hawler {
     uint256 toll = 100000;
-    function deposit(bytes32 _hashedPassword)external payable returns(address){
+    event Deposit(address sender, address withdrawAddress);
+    function deposit(bytes32 _hashedPassword)external payable{
         require(msg.sender != address(0), "Address 0");
         require(msg.value > toll, "Amount should be > toll");
         bytes32 _withdrawalHash = keccak256(abi.encodePacked(_hashedPassword));
         HawlerWithdrawer _contractWithdrawer = new HawlerWithdrawer(_withdrawalHash);
         (bool success, ) = address(_contractWithdrawer).call{value: msg.value-toll}("");
         require(success, "Unsuccessful");
-        return address(_contractWithdrawer);
+        emit Deposit(msg.sender, address(_contractWithdrawer));
     }
 }
